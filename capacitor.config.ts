d0f4d1e7e94b0bbd38f6fc2f9e2acec066257459
Miltos <miltos@localhost>
2025-12-26 22:11:00 +0200

    Initialize Capacitor and Android platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gymbody.ai',
+  appName: 'GymBodyAI',
+  webDir: 'dist'
+};
+
+export default config;
