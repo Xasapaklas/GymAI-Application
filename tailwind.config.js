270a17ac61fc9476ec95d02772d128eea7b04d67
Miltos <miltos@localhost>
2025-12-31 10:35:15 +0200

    Fix UI: Add missing Tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,5 +35,8 @@ export default {
             }
         }
     },
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/container-queries'),
+    ],
 }
